ff1de150a7817693e995e91bd187b7d203815859
bonfry <info@bonfry.com>
2023-08-18 11:08:12 +0200

    restyle footer

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,26 @@
 export class WebsiteConfig {
+    /*--------------------------------------------
+    |              GENERAL SETTINGS              |  
+    --------------------------------------------*/
+    
     public static readonly DEVFEST_NAME: string = 'Devfest Bari 2023';
     public static readonly DEVFEST_LOGO: string = '/assets/images/logo.png';
+
+    /*--------------------------------------------
+    |              SOCIAL SHARE                  |  
+    --------------------------------------------*/
+
+    public static readonly TWITTER_SOCIAL_TEXT : string = 'Devfest Bari 2023 {url}';
+    public static readonly TELEGRAM_MESSAGE_TEXT : string = 'Devfest Bari 2023';
+
+    /*--------------------------------------------
+    |              SOCIAL PAGES                  |  
+    --------------------------------------------*/
+
+    public static readonly FACEBOOK_PAGE : string = 'https://www.facebook.com/GDGBari';
+    public static readonly MEETUP_PAGE : string = '';
+    public static readonly INSTAGRAM_PAGE : string = 'https://www.instagram.com/gdgbari/';
+    public static readonly LINKEDIN_PAGE : string = 'https://www.linkedin.com/company/gdgbari/';
+    public static readonly WEBSITE : string = 'https://gdg.community.dev/gdg-bari/';
+
 }
